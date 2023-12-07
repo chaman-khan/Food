294dd733554cbcba10691e02fbdf8b843dcc1f14
chaman-khan <139955389+chaman-khan@users.noreply.github.com>
2023-12-08 04:45:35 +0500

    Updated

--- src/screens/SignUp_Screen/signupVerify.js
+++ src/screens/SignUp_Screen/signupVerify.js
@@ -44,7 +44,7 @@ const SignupVerify = ({navigation}) => {
       dispatch(authLoad(true));
 
       var raw = JSON.stringify({
-        otp: '9640',
+        otp: '5465',
       });
 
       console.log(raw);
