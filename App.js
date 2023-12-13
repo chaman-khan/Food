ae20df194f0b9bbc8cc34bd86812ae5aa05ca98d
AbdullahJaspal <abdullahafzal4444@gmail.com>
2023-12-13 18:23:00 +0500

    lassaa

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNav from './src/Navigations/mainNav';
 import {SafeAreaView} from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 const App = () => {
+  const [permissions, setPermissions] = useState({});
+
+  useEffect(() => {
+    const type = 'notification';
+    PushNotificationIOS.addEventListener(type, onRemoteNotification);
+    return () => {
+      PushNotificationIOS.removeEventListener(type);
+    };
+  });
+
+  const onRemoteNotification = notification => {
+    const isClicked = notification.getData().userInteraction === 1;
+
+    if (isClicked) {
+      // Navigate user to another screen
+    } else {
+      // Do something else with push notification
+    }
+    // Use the appropriate result based on what you needed to do for this notification
+    const result = PushNotificationIOS.FetchResult.NoData;
+    notification.finish(result);
+  };
   return (
     <SafeAreaView style={{flex: 1}}>
       <MainNav />
